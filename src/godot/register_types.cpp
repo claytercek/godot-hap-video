21a354d92d85c7b40f42fe77e6c241c4d6153ab8
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2026-07-13 10:28:41 -0400

    refactor(godot): delete unused HapTexture2D
    Registered publicly but never instantiated; leftover from the
    pre-Texture2DRD upload path.

--- src/godot/register_types.cpp
+++ src/godot/register_types.cpp
@@ -2,7 +2,6 @@
 
 #include "hap_player.h"
 #include "hap_resource_format_loader.h"
-#include "hap_texture_2d.h"
 #include "hap_video_stream.h"
 #include "hap_video_stream_playback.h"
 
@@ -23,7 +22,6 @@ void initialize_hap_video_module(ModuleInitializationLevel p_level) {
     return;
   }
 
-  ClassDB::register_class<HapTexture2D>();
   ClassDB::register_class<HapVideoStream>();
   ClassDB::register_class<HapVideoStreamPlayback>();
   ClassDB::register_class<HapPlayer>();
